194cd79cd95d915e551040b660dc6b97d0e22153
Mehdi Khaleqi <rotex2021rx@gmail.com>
2026-02-10 15:01:46 +0330

    add db factory

---
+++ Infrastructure/Persistence/AppDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.Persistence
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=MotoXDb;User Id=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=True;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
