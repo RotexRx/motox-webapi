e813b9c250a6b505361a2a02ac1b0e6e0d800469
Mehdi Khaleqi <rotex2021rx@gmail.com>
2026-02-13 03:15:43 +0330

    fully remove bikes

--- Application/Features/Bikes/Queries/GetLatestBikes/GetBikesQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using System.Collections.Generic;
-
-namespace MotoX.Application.Features.Bikes.Queries.GetLatestBikes;
-
-public record GetBikesQuery(int Count) : IRequest<List<BikeDto>>;
